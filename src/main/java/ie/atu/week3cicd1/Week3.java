60858ccc1c4aef9d0050ad19a744ac73bbe3fda9
Choon Xiang <g00425056@atu.ie>
2024-10-01 10:56:17 +0100

    Added Week3 class with getMessage and getDetails methods

---
+++ src/main/java/ie/atu/week3cicd1/Week3.java
@@ -0,0 +1,13 @@
+package ie.atu.week3cicd1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/person")
+public class Week3 {
+    @GetMapping("/message")
+    public String getMessage(){ return "Some message"; }
+    @GetMapping("/details")
+    public String getDetails(){ return "Some other message"; }
+}
