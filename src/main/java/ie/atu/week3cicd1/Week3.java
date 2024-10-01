6c09f4f023fad7bafd89c14699419870b6ceeeb6
Choon Xiang <g00425056@atu.ie>
2024-10-01 11:09:19 +0100

    Added greetByName method

--- src/main/java/ie/atu/week3cicd1/Week3.java
+++ src/main/java/ie/atu/week3cicd1/Week3.java
@@ -1,5 +1,7 @@
 package ie.atu.week3cicd1;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class Week3 {
     @GetMapping("/message")
     public String getMessage(){ return "Some message"; }
+
     @GetMapping("/details")
     public String getDetails(){ return "Some other message"; }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name){
+        return "Greetings, " + name;
+    }
 }
